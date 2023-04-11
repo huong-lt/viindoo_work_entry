df8d3c67376e6ce681a9ccb69fb861e7ff0b913e
huong <huong@huong-Darter-Pro>
2023-04-11 18:36:42 +0700

    [WIP] Update code

--- viin_hr_work_entries/__manifest__.py
+++ viin_hr_work_entries/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','viin_hr','hr_work_entry'],
+    'depends': ['base','viin_hr','hr_work_entry', 'hr_work_entry_contract'],
 
     # always loaded
     'data': [
