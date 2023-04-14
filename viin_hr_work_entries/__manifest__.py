89612c0717778885d93e36deb3cc687e9f664952
huong <huong@huong-Darter-Pro>
2023-04-14 07:30:24 +0700

    [WIP] Update menu

--- viin_hr_work_entries/__manifest__.py
+++ viin_hr_work_entries/__manifest__.py
@@ -1,26 +1,51 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Viin Work Entries",
+    'name_vi_VN': "Nhật ký công việc",
 
     'summary': """
-        Manageing work entry of employee""",
+        Manage work entry of employee""",
+    'summary_vi_VN': """
+        Quản lý công việc của nhân viên
+        """,    
 
     'description': """
-        Long description of module's purpose
+What is does
+============
+This module is managing work entry of employee
+
+Editions Supported
+==================
+1. Community Edition
+2. Enterprise Edition
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'description_vi_VN': """
+Module này làm gì
+=================
+Module này quản lý công việc của nhân viên.
+
+Ấn bản được Hỗ trợ
+==================
+1. Ấn bản Community
+2. Ấn bản Enterprise
+    """,
+
+    'author': "Viindoo",
+    'website': "https://viindoo.com",
+    'live_test_url': "https://v15demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v15demo-vn.viindoo.com",
+    'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'HR',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base','viin_hr','hr_work_entry', 'hr_work_entry_contract'],
-
+    
     # always loaded
     'data': [
         'security/hr_work_entry_security.xml',
@@ -35,5 +60,6 @@
     'demo': [
         'demo/demo.xml',
     ],
-     'application': True,
+    'images' : ['static/description/main_screenshot.png'],
+    'application': True,
 }
